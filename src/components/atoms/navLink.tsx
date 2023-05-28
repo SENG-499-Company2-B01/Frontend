7d708731b138e4f29b4e0856ee3764bb95457bf1
viviennezeng <viviennezeng7563@gmail.com>
2023-05-28 13:24:36 -0700

    Create styled link for nav bar

---
+++ src/components/atoms/navLink.tsx
@@ -0,0 +1,48 @@
+import styled from '@emotion/styled'
+import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+
+// export const NavUnlisted = styled.ul`
+
+//   display: flex;
+
+//   a {
+//     text-decoration: none;
+//   }
+
+//   li {
+//     color: red;
+//     margin: 0 0.8rem;
+//     font-size: 1.3rem;
+//     position: relative;
+//     list-style: none;
+//     &:hover {
+//       color: 'blue',
+//       text-decoration: 'underline',
+//       border-bottom: 2px solid #339af0
+//     }
+//   }
+
+//   .current {
+//     li {
+//       border-bottom: 2px solid black,
+//     }
+//   }
+// `
+
+export const NavUnlisted = styled(NavLink)`
+    color: black;
+    text-decoration: none;
+    &:hover {
+        text-decoration: underline;
+        color: blue;
+    }
+    &:active {
+        text-decoration: underline;
+        color: red;
+    }
+    &.active {
+        text-decoration: underline;
+        color: blue;
+    }
+`
