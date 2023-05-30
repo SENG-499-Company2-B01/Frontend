15bf7a27819e8fc4babc564b5dc353a91281a2fb
Bear <40529665+BearSpry@users.noreply.github.com>
2023-05-30 00:21:52 -0700

    Create input field component and add to practice page

---
+++ src/components/atoms/input_field.tsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled'
+
+export const InputField = styled.input`
+    height: 45px;
+    width: 304px;
+    left: -2235px;
+    top: -1471px;
+    border-radius: 8px;
+    padding: 8px 16px 8px 16px;
+`
