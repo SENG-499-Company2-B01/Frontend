9be9f402453db92efba71f4da59827683606aeeb
Edra Prasetio <edraadrianp@gmail.com>
2023-06-13 21:15:52 -0700

    Add username and password states

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,10 +4,35 @@ import { H1, H2 } from '../components/atoms/typography'
 import { InputField } from '../components/atoms/input_field'
 import { BlackButton } from '../components/atoms/button'
 import { NavUnlisted, SimpleLink } from '../components/atoms/navLink'
+import { useState } from 'react'
 
 export const Login = () => {
+    const [username, setUsername] = useState()
+    const [password, setPassword] = useState()
+    const [userType, setUserType] = useState()
+
+    function login(e: any) {
+        e.preventDefault()
+        const url = 'http://localhost:8000/' + 'login'
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password,
+            }),
+        })
+            .then((response) => {
+                return response.json()
+            })
+            .then((data) => {
+                console.log(data)
+            })
+    }
     return (
-        <div>
+        <form onSubmit={login}>
             <LoginBackground>
                 <TitleWrapper>
                     <LogoWrapper src={companyLogo} />
@@ -15,9 +40,21 @@ export const Login = () => {
                 </TitleWrapper>
                 <InputWrapper>
                     <H1>NetLink ID:</H1>
-                    <InputField placeholder='jsmith' />
+                    <InputField
+                        placeholder='jsmith'
+                        value={username}
+                        onChange={(e: any) => {
+                            setUsername(e.target.value)
+                        }}
+                    />
                     <H1>Password:</H1>
-                    <InputField type='password' />
+                    <InputField
+                        type='password'
+                        value={password}
+                        onChange={(e: any) => {
+                            setPassword(e.target.value)
+                        }}
+                    />
                 </InputWrapper>
                 <SimpleLink to='/user'>
                     <BlackButton style={{ width: '260px' }}>
@@ -28,6 +65,6 @@ export const Login = () => {
                     <H1>Forgot your password?</H1>
                 </NavUnlisted>
             </LoginBackground>
-        </div>
+        </form>
     )
 }
