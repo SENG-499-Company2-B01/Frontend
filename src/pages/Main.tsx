f243401ea9f9a5cdfc4f0ca71138d588b3fb3b51
Vikrant Katoch <vikrantkatoch2000@gmail.com>
2023-07-05 16:05:17 -0700

    finished the view (#31)

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -9,6 +9,7 @@ import { ProfPreferencePage } from './ProfPreferencePage'
 import { LogoutPage } from './LoggedOut'
 import { Generate } from './Generate'
 import { NewPreferences } from './NewPreferences'
+import { ProfTimetable } from './ProfTimetable'
 
 // Routes the web application to different pages
 
@@ -35,6 +36,14 @@ const Main = () => (
             element={
                 <>
                     <CreateAccountPage />
+                </>
+            }
+        />
+        <Route
+            path='/profview'
+            element={
+                <>
+                    <ProfTimetable />
                 </>
             }
         />
