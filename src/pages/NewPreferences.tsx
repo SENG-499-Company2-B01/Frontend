8536273f11ef9e344eeadb65d7d9ed2263b7ce52
Edra Prasetio <edraprasetio@w134-87-186-137.wireless.uvic.ca>
2023-07-06 13:23:58 -0700

    Move Search bar

--- src/pages/NewPreferences.tsx
+++ src/pages/NewPreferences.tsx
@@ -1,14 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import '../components/Homepage/homepage.css'
 import { NavBarAdmin } from '../components/navbar'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './test.css'
 import ProfTable from './ProfTable'
 import axios from 'axios'
+import PreLoader from '../components/Loading/PreLoader'
 
 export const NewPreferences: React.FC = () => {
+    const [loading, setLoading] = useState(false)
     useEffect(() => {
         const fetchData = async () => {
+            setLoading(true)
             try {
                 // eslint-disable-next-line @typescript-eslint/no-unused-vars
                 const response = await axios
@@ -38,6 +41,7 @@ export const NewPreferences: React.FC = () => {
                         })
                         const data = response_1.data
                         localStorage.setItem('dat', JSON.stringify(data))
+                        setLoading(false)
                     })
             } catch (error) {
                 console.error('Error:', error)
@@ -53,6 +57,7 @@ export const NewPreferences: React.FC = () => {
             <div className='con d-flex flex-row justify-content-between'>
                 <h1 className='hea_1'>Professors</h1>
             </div>
+            {loading ? <PreLoader /> : ''}
             <ProfTable list={parsedData} />
         </div>
     )
