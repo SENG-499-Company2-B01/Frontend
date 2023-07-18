732675c67805d61e88bc2834e6cf4d344a9af43c
Bear <40529665+BearSpry@users.noreply.github.com>
2023-07-18 10:05:50 -0700

    Fix loading screen

--- src/components/Loading/PreLoader.tsx
+++ src/components/Loading/PreLoader.tsx
@@ -1,15 +1,27 @@
 import './Loading.css'
+import styled from '@emotion/styled'
+
+const LoadingBackground = styled.div`
+    position: absolute;
+    display: flex;
+    width: 100%;
+    height: 100%;
+    background-color: #fcfc11;
+    justify-content: center;
+    align-items: center;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+`
 
 const PreLoader = () => {
     return (
-        <div className='lo'>
-            <div className='container'>
-                <div className='load'>
-                    <div className='ball'></div>
-                    <div className='text'>LOADING</div>
-                </div>
+        <LoadingBackground>
+            <div className='load'>
+                <div className='ball'></div>
+                <div className='text'>LOADING</div>
             </div>
-        </div>
+        </LoadingBackground>
     )
 }
 
