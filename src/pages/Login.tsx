bc3fe59bed57f6795bf05a0c3ca385d764ec326c
Erik McKelvey <Erik.McKelvey.is@gmail.com>
2023-08-03 22:25:19 -0700

    Added error handling for login page (#60)
    * started fix
    
    * Completed error handling for login
    
    * Fixed temp code for usertype
    
    * removed redundant check

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,27 +3,23 @@ import companyLogo from '../assets/icons/company_logo_dark.png'
 import { H1, H2 } from '../components/atoms/typography'
 import { InputField } from '../components/atoms/input_field'
 import { BlackButton } from '../components/atoms/button'
-import { NavUnlisted, SimpleLink } from '../components/atoms/navLink'
-import { Navigate } from 'react-router-dom'
-import { SyntheticEvent, useState, useEffect } from 'react'
-import { Home } from './Home'
-import Preloader from '../components/Loading/PreLoader'
-import { ProfHomepage } from './ProfHomepage'
-import { AdminHomepage } from './AdminHomepage'
+import { NavUnlisted } from '../components/atoms/navLink'
+import { useNavigate } from 'react-router-dom'
+import { SyntheticEvent, useState } from 'react'
 import PreLoader from '../components/Loading/PreLoader'
 
 export const Login = () => {
+    const navigate = useNavigate()
+
     const [username, setUsername] = useState()
     const [password, setPassword] = useState()
     const [loading, setLoading] = useState(false)
-    const [navigate, setNavigate] = useState(false)
+    const [errorMessage, setErrorMessage] = useState('')
 
     const login = async (e: SyntheticEvent) => {
+        setErrorMessage('')
         e.preventDefault()
         setLoading(true)
-
-        // console.log(username)
-        // console.log(password)
 
         const url = process.env.REACT_APP_BACKEND_URL + '/login'
 
@@ -39,30 +35,39 @@ export const Login = () => {
             }),
         })
             .then((response) => {
-                setLoading(false)
-                return response.json()
-            })
-            .then((data) => {
-                localStorage.setItem('jwt', data.jwt)
-                localStorage.setItem('status', 'login')
-                localStorage.setItem('user', 'prof')
-                if (data.usertype && data.usertype == 'admin') {
-                    localStorage.setItem('user', 'admin')
+                if (response.ok) {
+                    return response.json()
+                } else {
+                    throw new Error('Invalid username or password')
                 }
             })
-
-        if (username == 'Rich.Little') {
-            console.log('admin!')
-            localStorage.setItem('user', 'admin')
-        }
-
-        localStorage.setItem('username', String(username))
-        setNavigate(true)
-        console.log(localStorage)
-    }
-
-    if (navigate) {
-        return <Navigate to='/user' />
+            .then((data) => {
+                if (!data.token) {
+                    throw new Error('Invalid username or password')
+                }
+                localStorage.setItem('jwt', data.token)
+                localStorage.setItem('status', 'login')
+                localStorage.setItem('username', String(username))
+                if (data.usertype == 'admin') {
+                    console.log('admin!')
+                    localStorage.setItem('user', 'admin')
+                } else {
+                    console.log('prof!')
+                    localStorage.setItem('user', 'prof')
+                }
+                navigate('/user')
+            })
+            .catch((error) => {
+                console.log(error)
+                if (error.message) {
+                    setErrorMessage(error.message)
+                } else {
+                    setErrorMessage('An error has occurred')
+                }
+            })
+            .finally(() => {
+                setLoading(false)
+            })
     }
 
     return (
@@ -72,6 +77,7 @@ export const Login = () => {
                     <LogoWrapper src={companyLogo} />
                     <H2>LOG IN</H2>
                 </TitleWrapper>
+                <H1 style={{ color: 'red' }}>{errorMessage}</H1>
                 <InputWrapper>
                     <H1>Username:</H1>
                     <InputField placeholder='John.Smith' value={username} required onChange={(e: any) => setUsername(e.target.value)} />
@@ -79,6 +85,7 @@ export const Login = () => {
                     <InputField
                         type='password'
                         value={password}
+                        required
                         onChange={(e: any) => {
                             setPassword(e.target.value)
                         }}
